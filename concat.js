e2e5d9cd5e2682742b2dcfcc41a4501334a9bafa
EngMehed <mehedhasan87@gmail.com>
2021-11-08 17:54:19 +0600

    concat update

---
+++ concat.js
@@ -0,0 +1,10 @@
+const x = [1, 2, 3, 4, 5, 6]
+const y = [7, 8, 9, 10, 11, 12]
+
+console.log(result = x.concat(y)); 
+
+const letters = ['a', 'b', 'c'];
+
+const alphaNumeric = letters.concat(1, [2, 3]);
+
+console.log(alphaNumeric);
